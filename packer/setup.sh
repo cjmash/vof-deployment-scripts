5f1497d2f344a455d4be4839ddb22db24c45fdb8
Collins Macharia <jamescollins.jcparty@gmail.com>
2018-04-05 12:12:29 +0300

    Update setup.sh

--- packer/setup.sh
+++ packer/setup.sh
@@ -14,6 +14,8 @@ echo "creating users"
 }
 install_nginx() {
 echo "installing nginx"
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock
 sudo apt-get update
 sudo apt-get install nginx -y
 
